5125e5c0812e03aa7b611fb1c97fb5fa0824d8f7
developerdialogs <62574469+developerdialogs@users.noreply.github.com>
2020-05-09 19:51:44 -0700

    make ThemeExample generic

--- ThemeExample.playground/Contents.swift
+++ ThemeExample.playground/Contents.swift
@@ -3,81 +3,80 @@
 import SwiftUI
 import PlaygroundSupport
 
-struct ThemeExampleDemo: View {
+struct ThemeExampleDemo<T: View, U: View, V: View>: View {
+    private let headerWithCaptionViewProvider: (Binding<String>, Binding<String>) -> T
+    private let contentViewProvider: (Binding<String>, Binding<String>) -> U
+    private let primaryButton: V
+    private let secondaryButton: V
+    private let tertiaryButton: V
     @EnvironmentObject var theme: MaterialTheme
-    @State var headlineTitle = "Theme Example"
-    @State private var headlineCaption = "Let's get the themes changing!"
 
+    @State private var headlineTitle = "Theme Example"
+    @State private var headlineCaption = "Let's get the themes changing!"
     @State private var contentTitle = "What are Themes"
     @State private var contentText = "Themes let you apply a consistent design scheme to your app. It allows you to customize all design aspects of your project."
+
+    init(
+        headerWithCaptionViewProvider: @escaping (Binding<String>, Binding<String>) -> T,
+        contentViewProvider: @escaping (Binding<String>, Binding<String>) -> U,
+        primaryButton: (String) -> V,
+        secondaryButton: (String) -> V,
+        tertiaryButton: (String) -> V
+    ) {
+        self.headerWithCaptionViewProvider = headerWithCaptionViewProvider
+        self.contentViewProvider = contentViewProvider
+        self.primaryButton = primaryButton("Red Theme")
+        self.secondaryButton = secondaryButton("White Theme")
+        self.tertiaryButton = tertiaryButton("Switch Dark Mode")
+    }
+
     var body: some View {
         return ZStack {
             VStack {
-                HeadlineWithCaption(title: $headlineTitle, caption: $headlineCaption)
+                headerWithCaptionViewProvider($headlineTitle, $headlineCaption)
                 Spacer()
                 Image(uiImage: UIImage(named: "image")!)
-                .resizable()
-                .clipShape(Circle())
-                .frame(width: 200, height: 200)
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 200, height: 200)
                     .shadow(color: theme.colorPalette.secondary, radius: 10.0, x: 0, y: 0)
+
                 Spacer()
                 HStack(spacing: 16.0) {
                     Spacer()
-                    TitleWithText(title: $contentTitle, text: $contentText)
+                    contentViewProvider($contentTitle, $contentText)
                     Spacer()
                 }
-                
                 HStack(spacing: 16.0) {
-                    ActionButton(title: "Primary", type: .primary)
-                    ActionButton(title: "Secondary", type: .secondary)
+                    primaryButton
+                    secondaryButton
                 }.padding(.top, 24.0)
-                
-                ActionButton(title: "Link", type: .tertiary)
+                tertiaryButton
                     .padding(.top, 12.0)
-                
                 Spacer()
             }.padding(.top, 32.0)
         }
     }
 }
 
-
-
 let defaultTheme = MaterialTheme()
 
 var env = defaultTheme
 
-let view  = ThemeExampleDemo().environmentObject(env)
-PlaygroundPage.current.liveView = UIHostingController(rootView: view)
-
-
-
-//let redColorPalette = MaterialTheme.ColorPalette(
-//    primary: .red,
-//    secondary: .red,
-//    primaryVariant: .red,
-//    secondaryVariant: .red,
-//    background: .red,
-//    onSecondary: .black,
-//    onBackground: .red,
-//    onSurface: .red)
-//
-//let redTypography = MaterialTheme.Typography(
-//    h1: Font.system(size: 40).italic(),
-//    h2: Font.system(size: 36).italic(),
-//    h3: Font.system(size: 32).italic(),
-//    h4: Font.system(size: 24).italiTc(),
-//    h5: Font.system(size: 20).italic(),
-//    h6: Font.system(size: 18).italic())
-//var currentTheme = "black"
-//Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { _ in
-//    if currentTheme == "red" {
-//        currentTheme = "black"
-//        env.colorPalette = MaterialTheme.ColorPalette()
-//        env.typography = MaterialTheme.Typography()
-//    } else {
-//        currentTheme = "red"
-//        env.colorPalette = redColorPalette
-//        env.typography = redTypography
-//    }
-//}
+let view  = ThemeExampleDemo(
+    headerWithCaptionViewProvider: { title, caption in
+        return HeadlineWithCaption(title: title, caption: caption)
+},
+    contentViewProvider: { title, text in
+        TitleWithText(title: title, text: text)
+},
+    primaryButton: { title in
+        ActionButton(title: title, type: .primary)
+},
+    secondaryButton: { title in
+        ActionButton(title: title, type: .secondary)
+},
+    tertiaryButton: { title in
+        ActionButton(title: title, type: .tertiary)
+})
+PlaygroundPage.current.liveView = UIHostingController(rootView: view.environmentObject(env))
